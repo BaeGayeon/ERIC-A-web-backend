af991afb9a84d8a6e07efc6f22f401153bfaaedd
BaeGayeon <ebgy918@gmail.com>
2023-08-06 11:20:12 +0900

    feat: deliveryController, Service 에러 처리

--- services/deliveryService.js
+++ services/deliveryService.js
@@ -1,55 +1,66 @@
 const { delivery, user } = require('../models');
+const baseResponse = require('../config/baseResponseStatus');
+const { response, errResponse } = require('../config/response');
 
 module.exports = {
     createDelivery: async function (req, transaction) {
-        let today = new Date();
+        try {
+            let today = new Date();
 
-        let dateResult = today.toLocaleDateString();
-        let timeResult = today.toLocaleTimeString();
+            let dateResult = today.toLocaleDateString();
+            let timeResult = today.toLocaleTimeString();
 
-        console.log(dateResult, timeResult);
-
-        await delivery.create(
-            {
-                name: req.body.name,
-                phoneNumber: req.body.phoneNumber,
-                departure: req.body.departure,
-                destination: req.body.destination,
-                item: req.body.item,
-                inPerson: req.body.isInPerson,
-                status: req.body.status,
-                userId: req.body.userId,
-                date: dateResult,
-                time: timeResult,
-                isAccepted: req.body.isAccepted,
-            },
-            { transaction: transaction }
-        );
+            await delivery.create(
+                {
+                    name: req.body.name,
+                    phoneNumber: req.body.phoneNumber,
+                    departure: req.body.departure,
+                    destination: req.body.destination,
+                    item: req.body.item,
+                    inPerson: req.body.isInPerson,
+                    status: req.body.status,
+                    userId: req.body.userId,
+                    date: dateResult,
+                    time: timeResult,
+                    isAccepted: req.body.isAccepted,
+                },
+                { transaction: transaction }
+            );
+            return response(baseResponse.SUCCESS);
+        } catch (err) {
+            console.log(err);
+            return errResponse(baseResponse.DB_ERROR);
+        }
     },
     findDeliveryByPN: async function (phoneNumber) {
-        const deliveryResult = await delivery.findAll({
-            where: { phoneNumber: phoneNumber },
-        });
-        const result = [];
-        for (let i = 0; i < deliveryResult.length; i++) {
-            d = deliveryResult[i];
-
-            result.push({
-                id: d.dataValues.id,
-                name: d.dataValues.name,
-                phoneNumber: d.dataValues.phoneNumber,
-                destination: d.dataValues.destination,
-                isInPerson: d.dataValues.inPerson
-                    ? '직접 수령하기'
-                    : '두고 가기',
-                item: d.dataValues.item,
-                isAccepted: d.dataValues.isAccepted,
-                status: d.dataValues.status,
-                date: d.dataValues.date,
-                time: d.dataValues.time,
+        try {
+            const deliveryResult = await delivery.findAll({
+                where: { phoneNumber: phoneNumber },
             });
+            const result = [];
+            for (let i = 0; i < deliveryResult.length; i++) {
+                d = deliveryResult[i];
+
+                result.push({
+                    id: d.dataValues.id,
+                    name: d.dataValues.name,
+                    phoneNumber: d.dataValues.phoneNumber,
+                    destination: d.dataValues.destination,
+                    isInPerson: d.dataValues.inPerson
+                        ? '직접 수령하기'
+                        : '두고 가기',
+                    item: d.dataValues.item,
+                    isAccepted: d.dataValues.isAccepted,
+                    status: d.dataValues.status,
+                    date: d.dataValues.date,
+                    time: d.dataValues.time,
+                });
+            }
+            return response(baseResponse.SUCCESS, result);
+        } catch (err) {
+            console.log(err);
+            return errResponse(baseResponse.DB_ERROR);
         }
-        return result;
     },
     acceptDelivery: async function (id) {
         delivery.update(
